e107dbead15fef410a077215f9547dfe211fb3e9
virginia koar <virginiakoar@TimKoar.fios-router.home>
2019-03-20 21:16:10 -0400

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.map do |attendee|
+    badge_maker "#{attendee}"
+  end
+end
+
+def assign_rooms(array)
+  array.each_with_index.map do |attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+  end
+end
+
+def printer(array)
+  batch_badge_creator(array).each do |badge|
+    puts badge
+  end
+  assign_rooms(array).each do |assign|
+    puts assign
+  end
+end
